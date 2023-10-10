d309d5fa332258382c0a56bddb16daace4c98225
Pedro Horacio <129102505+Pedroh88@users.noreply.github.com>
2023-10-09 22:17:07 -0300

    commit inicial

---
+++ apigrupo06/src/main/java/br/com/api/g6/Apigrupo06Application.java
@@ -0,0 +1,13 @@
+package br.com.api.g6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apigrupo06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apigrupo06Application.class, args);
+	}
+
+}
---
+++ apigrupo06/src/test/java/br/com/api/g6/Apigrupo06ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.g6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Apigrupo06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
