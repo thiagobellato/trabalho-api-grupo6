776761322f771ddb6db28bf1732b637801b348b6
rblfa <digoberredo@gmail.com>
2023-10-12 18:28:34 -0300

    Feat: Criação da conexão entre entidades pedido e produto manytomany.

--- apigrupo06/src/main/java/br/com/api/g6/entities/Pedido.java
+++ apigrupo06/src/main/java/br/com/api/g6/entities/Pedido.java
@@ -1,11 +1,16 @@
 package br.com.api.g6.entities;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -14,25 +19,37 @@ public class Pedido {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="pedido_id")
 	private Integer id;
 	
+	@Column(name="pedido_produto")
 	private String produto;
+	@Column(name="pedido_quantidade")
 	private Integer quantidade;
+	@Column(name="pedido_date")
 	private Date data;
 	//private String comprador;
 	//private String vendedor;
 	
+	@ManyToMany
+	@JoinTable(name="pedido_produto",
+	joinColumns=@JoinColumn(name="pedido_id"),
+	inverseJoinColumns=@JoinColumn(name="produto_id"))
+		
+	private List<Produto> produtos;
+
 	public Pedido() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	
-	public Pedido(Integer id, String produto, Integer quantidade, Date data) {
+
+	public Pedido(Integer id, String produto, Integer quantidade, Date data, List<Produto> produtos) {
 		super();
 		this.id = id;
 		this.produto = produto;
 		this.quantidade = quantidade;
 		this.data = data;
+		this.produtos = produtos;
 	}
 
 	public Integer getId() {
@@ -67,9 +84,18 @@ public class Pedido {
 		this.data = data;
 	}
 
-	@Override
-	public String toString() {
-		return "Pedido [id=" + id + ", produto=" + produto + ", quantidade=" + quantidade + ", data=" + data + "]";
+	public List<Produto> getProdutos() {
+		return produtos;
 	}
 
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+	@Override
+	public String toString() {
+		return "Pedido [id=" + id + ", produto=" + produto + ", quantidade=" + quantidade + ", data=" + data
+				+ ", produtos=" + produtos + "]";
+	}
+	
 }
