f51fcee92e5eb1a1f61d1e793487edbe1e3801cf
Thiago Bellato <tfbellato@hotmail.com>
2023-10-12 17:42:56 -0300

    Feat: Criação da Entidade Produto e relacionamento manytoone.

--- apigrupo06/src/main/java/br/com/api/g6/entities/Categoria.java
+++ apigrupo06/src/main/java/br/com/api/g6/entities/Categoria.java
@@ -1,10 +1,15 @@
 package br.com.api.g6.entities;
 
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Table;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "categoria")
@@ -12,44 +17,54 @@ public class Categoria {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "categoria_id")
 	private Integer id;
-	// precisa pular linha para reconhecer a anotação?
-	
+
+	@Column(name = "categoria_nome")
 	private String nome;
+
+	@Column(name = "categoria_descricao")
 	private String descricao;
-	
+
 	public Categoria(Integer id, String nome, String descricao) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.descricao = descricao;
 	}
+
 	public Categoria() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	@Override
 	public String toString() {
 		return "Categoria [id=" + id + ", nome=" + nome + ", descricao=" + descricao + "]";
 	}
-	
-	
+
 }
--- apigrupo06/src/test/java/br/com/api/g6/Apigrupo06ApplicationTests.java
+++ apigrupo06/src/test/java/br/com/api/g6/Apigrupo06ApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.api.g6;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class Apigrupo06ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package br.com.api.g6;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class Apigrupo06ApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
