ea27bfda64ec35f0145b97606863f4aafe626cf3
Thiago Bellato <tfbellato@hotmail.com>
2023-10-14 00:06:56 -0300

    Feat: Criação e Modificações de todas as Interfaces.

--- apigrupo06/src/main/java/br/com/api/g6/repositories/CategoriaRepository.java
+++ apigrupo06/src/main/java/br/com/api/g6/repositories/CategoriaRepository.java
@@ -3,13 +3,11 @@ package br.com.api.g6.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import br.com.api.g6.entities.Categoria;
 
 @Repository 
 public interface CategoriaRepository extends JpaRepository<Categoria,Integer> {
 	
-	@Query(value="select count(*) from teste", nativeQuery = true)
+	@Query(value="select count(*) from g6marketplace", nativeQuery = true)
 	public Integer contar();
-
 }
