d16e345f82f65fe4b3843f7731f1c8a3efeaab8c
Thiago Bellato <tfbellato@hotmail.com>
2023-10-14 00:06:56 -0300

    Feat: Criação e Modificações de todas as Interfaces.

--- apigrupo06/src/main/java/br/com/api/g6/repositories/PedidoRepository.java
+++ apigrupo06/src/main/java/br/com/api/g6/repositories/PedidoRepository.java
@@ -3,12 +3,11 @@ package br.com.api.g6.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import br.com.api.g6.entities.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido,Integer>{
 
-	@Query(value="select count(*) from teste", nativeQuery = true)
+	@Query(value="select count(*) from g6marketplace", nativeQuery = true)
 	public Integer contar();
 }
--- apigrupo06/src/main/java/br/com/api/g6/repositories/UsuarioRepository.java
+++ apigrupo06/src/main/java/br/com/api/g6/repositories/UsuarioRepository.java
@@ -3,7 +3,6 @@ package br.com.api.g6.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import br.com.api.g6.entities.Usuario;
 
 @Repository 
@@ -11,5 +10,4 @@ public interface UsuarioRepository extends JpaRepository<Usuario,Integer> {
 	
 	@Query(value="select count(*) from g6marketplace", nativeQuery = true)
 	public Integer contar();
-
 }
