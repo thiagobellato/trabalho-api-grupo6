36981f9b602748d8aa387a1d47a9ca440477871f
Thiago Bellato <tfbellato@hotmail.com>
2023-10-14 00:06:56 -0300

    Feat: Criação e Modificações de todas as Interfaces.

--- apigrupo06/src/main/java/br/com/api/g6/repositories/EnderecoRepository.java
+++ apigrupo06/src/main/java/br/com/api/g6/repositories/EnderecoRepository.java
@@ -1,5 +1,13 @@
 package br.com.api.g6.repositories;
 
-public class EnderecoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import br.com.api.g6.entities.Endereco;
 
+@Repository 
+public interface EnderecoRepository extends JpaRepository<Endereco,Integer> {
+	
+	@Query(value="select count(*) from g6marketplace", nativeQuery = true)
+	public Integer contar();
 }
