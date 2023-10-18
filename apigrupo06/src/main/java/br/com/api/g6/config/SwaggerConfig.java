19d2465dd369ddd4b0c6e287da35ed6ca4b6a749
juliafclima <juliafclima@hotmail.com>
2023-10-18 18:52:10 -0300

    build: subindo versão

--- apigrupo06/src/main/java/br/com/api/g6/config/SwaggerConfig.java
+++ apigrupo06/src/main/java/br/com/api/g6/config/SwaggerConfig.java
@@ -55,7 +55,7 @@ public class SwaggerConfig {
 				.title("Documentação de API projeto final")
 				.description("Projeto final da disciplina de APIrestful")
 				.license("Apache license version 2.0")
-				.version("5.4.0")
+				.version("5.5.0")
 				.build();
 		return apiInfo;
 	}
