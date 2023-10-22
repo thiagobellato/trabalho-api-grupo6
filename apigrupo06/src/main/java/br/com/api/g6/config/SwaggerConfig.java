8e7eb0bcca2b84de903e3768b254d87be4257ae3
Thiago Bellato <tfbellato@hotmail.com>
2023-10-22 00:18:26 -0300

    Build: Subindo Versão

--- apigrupo06/src/main/java/br/com/api/g6/config/SwaggerConfig.java
+++ apigrupo06/src/main/java/br/com/api/g6/config/SwaggerConfig.java
@@ -49,7 +49,7 @@ public class SwaggerConfig {
 	private ApiInfo apiInfo() {
 		ApiInfo apiInfo = new ApiInfoBuilder().title("Documentação de API projeto final")
 				.description("Projeto final da disciplina de APIrestful").license("Apache license version 2.0")
-				.version("5.14.0").build();
+				.version("5.15.0").build();
 		return apiInfo;
 	}
 }
